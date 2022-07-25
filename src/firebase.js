3513b41c7b28182c1b7428c9bcc594712786c074
Charlie Cureton <ccureton21@gmail.com>
2022-07-25 19:50:02 +0100

    Firebase config file added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDxXb_Hphz-Bc-i2t_YxcHClZ68Q1iq6UQ",
+    authDomain: "clone-6f3f9.firebaseapp.com",
+    projectId: "clone-6f3f9",
+    storageBucket: "clone-6f3f9.appspot.com",
+    messagingSenderId: "905385855551",
+    appId: "1:905385855551:web:a66830ed1748d5ceacc68a",
+    measurementId: "G-0RNW1N11S8"
+  };

2eced015884acda0a1a03bd0c07cd040b373b3d3
Charlie Cureton <ccureton21@gmail.com>
2022-07-26 13:27:22 +0100

    Home page banner

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../css/Home.css';
+import Banner from '../assets/banner.webp';
+
+function Home() {
+  return (
+    <div className="home__container">
+        <img className="home__banner" src={Banner} alt="Home page banner"/>
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">
+          {/* Product */}
+        </div>
+    </div>
+  )
+}
+
+export default Home

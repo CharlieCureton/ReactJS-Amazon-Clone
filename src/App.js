4784dca51fd5a8c64e68a8750dddf79afc839d13
Charlie Cureton <ccureton21@gmail.com>
2022-07-26 11:34:07 +0100

    Header Content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
+      <Header />
+      {/* Home */}
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+  return (
+    <div className='header'>
+        <img src="./amazon_logo.webp" className="header__logo" alt="Amazon Logo" />
+        
+        <div className="header__search">
+          <input className="header__searchInput" type="text"/>
+          {/* Logo */}
+        </div>
+
+        <div className="header__nav">
+            <div className="header__option">
+              <span className="header__optionLineOne">
+                Hello Guest
+              </span>
+              <span className="header__optionLineTwo">
+                Sign In
+              </span>
+            </div>
+
+            <div className="header__option">
+              <span className="header__optionLineOne">
+                Returns
+              </span>
+              <span className="header__optionLineTwo">
+                &amp; Orders
+              </span>
+            </div>
+
+            <div className="header__option">
+              <span className="header__optionLineOne">
+                <span className="header__optionLineOne">
+                 Your
+                </span>
+                <span className="header__optionLineTwo">
+                  Prime
+                </span>
+              </span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
